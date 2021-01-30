6300f31de3065deb9d3abb7ec33e31c029a5a1c7
Luuk Verweij <luuk_verweij@msn.com>
2021-01-30 21:02:42 +0100

    feat: add mistakes param for /svg/boards/<id>

--- training/blueprints/svg/views.py
+++ training/blueprints/svg/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, Response, make_response
+from flask import Blueprint, Response, make_response, request
 
 from othello.board import BLACK, EMPTY, VALID_MOVE, WHITE, Board
+from typing import Set
 
 svg = Blueprint("svg", __name__)
 
@@ -16,7 +17,7 @@ def board_image(board_id: str) -> Response:
     cell_size = image_size / 8
     disc_radius = 0.38 * cell_size
     move_radius = 0.08 * cell_size
-    # cross_width = 0.3 * cell_size TODO
+    cross_width = 0.3 * cell_size
 
     body = f"""<?xml version="1.0"?>
     <svg width="{image_size}" height="{image_size}" xmlns="http://www.w3.org/2000/svg"
@@ -32,6 +33,17 @@ def board_image(board_id: str) -> Response:
         body += f"""<line x1="0" y1="{offset}" x2="{image_size}" y2="{offset}"
         style="stroke:black; stroke-width:2" />\n"""
 
+    mistakes = request.args.get("mistakes", "")
+    print(mistakes)
+
+    mistake_indexes: Set[int] = set()
+
+    for index in mistakes.split(","):
+        try:
+            mistake_indexes.add(int(index))
+        except ValueError:
+            pass
+
     for index, field in enumerate(board.get_fields()):
         circle_x = (cell_size / 2) + cell_size * (index % 8)
         circle_y = (cell_size / 2) + cell_size * (index // 8)
@@ -39,6 +51,25 @@ def board_image(board_id: str) -> Response:
         move_color = {BLACK: "black", WHITE: "white"}[board.turn]
 
         if field == VALID_MOVE:
+
+            if index in mistake_indexes:
+                cross_min_x = (
+                    (cell_size / 2) + cell_size * (index % 8) - (cross_width / 2)
+                )
+                cross_min_y = (
+                    (cell_size / 2) + cell_size * (index // 8) - (cross_width / 2)
+                )
+                cross_max_x = cross_min_x + cross_width
+                cross_max_y = cross_min_y + cross_width
+
+                body += f"""<line x1="{cross_min_x}" y1="{cross_min_y}"
+                x2="{cross_max_x}" y2="{cross_max_y}" style="stroke:red;
+                stroke-width:7" />\n
+                <line x1="{cross_max_x}" y1="{cross_min_y}"
+                x2="{cross_min_x}" y2="{cross_max_y}" style="stroke:red;
+                stroke-width:7" />\n"""
+                continue
+
             body += f"""<circle cx="{circle_x}" cy="{circle_y}"
             r="{move_radius}" fill="{move_color}" />\n"""
             continue
@@ -50,20 +81,6 @@ def board_image(board_id: str) -> Response:
 
         body += f"""<circle cx="{circle_x}" cy="{circle_y}"
         r="{disc_radius}" fill="{fill_color}" />\n"""
-
-    # TODO
-    # for index in wrongs:
-    #     cross_min_x = (cell_size / 2) + cell_size * (index % 8) - (cross_width / 2)
-    #     cross_min_y = (cell_size / 2) + cell_size * (index // 8) - (cross_width / 2)
-    #     cross_max_x = cross_min_x + cross_width
-    #     cross_max_y = cross_min_y + cross_width
-    #
-    #     body += f"""<line x1="{cross_min_x}" y1="{cross_min_y}"
-    #     x2="{cross_max_x}" y2="{cross_max_y}" style="stroke:red;
-    #     stroke-width:7" />\n
-    #     <line x1="{cross_max_x}" y1="{cross_min_y}"
-    #     x2="{cross_min_x}" y2="{cross_max_y}" style="stroke:red;
-    #     stroke-width:7" />\n"""
 
     body += "</svg>"
 
